00af74096db518b980fbe6b46c4a5938639b43c8
Hans Larsen <hans@slack-corp.com>
2014-09-21 11:29:21 -0700

    Specify the node hostname when running RabbitMQ. It seems the latest version likes this better, even if it's localhost.

--- habitat/components/rabbitmq.py
+++ habitat/components/rabbitmq.py
@@ -7,8 +7,9 @@ import os
 class RabbitMqServer(ServerBase):
     server_bin = 'rabbitmq-server'
     server_args = []
+    rabbitmq_node_hostname = '%(hostname)s'
     server_env = {
-        'RABBITMQ_NODENAME': '%(rabbitmq_nodename)s',
+        'RABBITMQ_NODENAME': '%(rabbitmq_nodename)s@%(rabbitmq_node_hostname)s',
         'RABBITMQ_NODE_IP_ADDRESS': '%(hostname)s',
         'RABBITMQ_NODE_PORT': '%(port)s',
     }
